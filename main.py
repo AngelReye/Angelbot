f675b7f7aab2110016ecf94055c1a50f748d6ac3
AngelReye <Luisre07@gmail.com>
2025-05-30 21:40:33 -0500

    main.py

--- main.py
+++ main.py
@@ -44,4 +44,36 @@ async def repeat(ctx, times: int, content='repeating...'):
     for i in range(times):
         await ctx.send(content)
 
+@bot.command()
+async def meme(ctx):
+    imagenes = os.listdir('images')
+    seleccion = random.choice(imagenes)
+    with open(f'images/{seleccion}', 'rb') as f:
+        picture = discord.File(f)
+        await ctx.send(file=picture)
+
+def get_duck_image_url():
+    url = 'https://random-d.uk/api/random'
+    res = requests.get(url)
+    data = res.json()
+    return data['url']
+
+@bot.command('duck')
+async def duck(ctx):
+    '''Una vez que llamamos al comando duck,
+    el programa llama a la función get_duck_image_url'''
+    image_url = get_duck_image_url()
+    await ctx.send(image_url)
+
+def get_dog_image_url():
+    url = 'https://random.dog/woof.json'
+    res = requests.get(url)
+    data = res.json()
+    return data['url']
+
+@bot.command('dog')
+async def dog(ctx):
+    image_url = get_dog_image_url()
+    await ctx.send(image_url)
+
 bot.run(TOKEN)
